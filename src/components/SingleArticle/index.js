0d149ced46cd0a4407c01519247403851f4f1ec1
Tameka A <56846452+taaych@users.noreply.github.com>
2023-07-05 16:35:31 +0000

    Update index.js and index.js

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,18 +1,20 @@
+import { useParams } from 'react-router-dom';
 import './SingleArticle.css';
 
-const SingleArticle = () => {
+const SingleArticle = ({ articles }) => {
+  const params = useParams();
+  
+  const article = articles.find(({ id }) => id === params.id);
+  
   return (
     <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
+      <h1>{article?.title}</h1>
       <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
-        alt='home'
+        src={article?.imageUrl}
+        alt={article?.title}
       />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
+        {article?.body}
       </p>
     </div>
   );
